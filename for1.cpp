2be0fe0a6a1fbf60fe8d8e7a244c2a347beef8a2
Mint Bao <minhbao1223@gmail.com>
2021-11-25 21:52:29 +0700

    hi em oi

--- for1.cpp
+++ for1.cpp
@@ -6,7 +6,8 @@ int main() {
     int n;
     cin >>n;
     cout <<"Hello anh bao dep trai";
-    cout <<"Hello anh bao cute hehe ";
+    cout <<"Hello anh bao cute hehe hahahahahahahahhahahahah ";
+    cout <<"Hello anh bao ";
     for (int i=1 ; i<=n ; i++) {
         cout<<i<<" ";
     }
