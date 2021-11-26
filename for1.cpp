3e31be0929211dc013c456772175541b13fbd94e
minhbaone <86735522+minhbaone@users.noreply.github.com>
2021-11-26 11:34:10 +0700

    Update for1.cpp

--- for1.cpp
+++ for1.cpp
@@ -5,8 +5,6 @@ using namespace std;
 int main() {
     int n;
     cin >>n;
-    cout <<"Hello anh bao dep trai";
-    cout <<"Hello  ";
     cout <<"Hello anh bao ";
     for (int i=1 ; i<=n ; i++) {
         cout<<i<<" ";
